a26ba78bd0121f129137894322886b0be9e6ff9d
Shaun Wassell <shaun@trove.ai>
2021-06-17 10:35:10 -0400

    Added pages and client-side routes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,35 @@
+import { BrowserRouter, Switch, Route, Link } from 'react-router-dom';
+import { Home } from './pages/Home';
+import { About } from './pages/About';
+import { Articles } from './pages/Articles';
+
 const App = () => {
 	return (
-		<h1>Server-Side Rendering Example</h1>
+		<BrowserRouter>
+			<h1>Server-Side Rendering Example</h1>
+			<ul>
+				<li>
+					<Link to="/">Home</Link>
+				</li>
+				<li>
+					<Link to="/about">About</Link>
+				</li>
+				<li>
+					<Link to="/articles">Articles</Link>
+				</li>
+			</ul>
+			<Switch>
+				<Route path="/" exact>
+					<Home />
+				</Route>
+				<Route path="/about">
+					<About />
+				</Route>
+				<Route path="/articles">
+					<Articles />
+				</Route>
+			</Switch>
+		</BrowserRouter>
 	);
 }
 
